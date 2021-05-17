ce8c3978eba1f22354aaf957ffe13b36f4d506d5
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-17 23:34:20 +0530

    ADD:Find cheapest hotel in given date range

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,35 +1,77 @@
 package com.HotelReservationSystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HotelReservationSystem {
 	
 	private static Hotel Lotus,RadhaKirshna,Ganesh;
-	public static void main(String args[])
-	{
-		HotelDetails();
-	}
-	
-	public static void HotelDetails()
-	{
-		System.out.println("Hotel and price for regular customer");
-		
-		Lotus=new Hotel("Lotus");
-		Lotus.setRegularWeekDay(150);
-		Lotus.setRegularWeekEnd(100);
-		
-		System.out.println("Hotel Name:"+Lotus.getHotelName()+"\n"+"Regular WeekDay price:"+Lotus.getRegularWeekDay()+"\n"+"Regular week End price:"+Lotus.getRegularWeekEnd()+ "\n");
-		
-		RadhaKirshna=new Hotel("RadhaKirshna");
-		RadhaKirshna.setRegularWeekDay(200);
-		RadhaKirshna.setRegularWeekEnd(150);
-		
-		System.out.println("Hotel Name:"+RadhaKirshna.getHotelName()+"\n"+"Regular WeekDay price:"+RadhaKirshna.getRegularWeekDay()+"\n"+"Regular week End price:"+RadhaKirshna.getRegularWeekEnd()+ "\n");
-		
-		Ganesh=new Hotel("Ganesh");
-		Ganesh.setRegularWeekDay(200);
-		Ganesh.setRegularWeekEnd(150);
-		
-		System.out.println("Hotel Name:"+Ganesh.getHotelName()+"\n"+"Regular WeekDay price:"+Ganesh.getRegularWeekDay()+"\n"+"Regular week End price:"+Ganesh.getRegularWeekEnd()+ "\n");
-	}
+	public static void main(String[] args) throws IOException {
+	    
+   	 System.out.println("Enter date  ex. 17May2021(mon)");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String date = bf.readLine();
 
+		
+		//setting values for lakewood
+	    Lotus = new Hotel("Lotus");
+	    Lotus.setRegularWeekDay(100);
+	    
+		 		
+		//setting values for bridgewood
+	   RadhaKirshna = new Hotel("RadhaKirshna");
+	   RadhaKirshna.setRegularWeekDay(150);
+	   
+				
+		//setting values for ridgewood   
+	   Ganesh = new Hotel("Ganesh");
+	   Ganesh.setRegularWeekDay(250);
+	    
+		
+		 //int index = date.indexOf(":");
+	     //String type = date.substring(0, index);
+	     int cost_Lotus = 0, cost_RadhaKirshna = 0, cost_Ganesh = 0;
+	     int day_index_start = 0, day_index_end = 0;
+	     
+	    while (day_index_start != -1) {
+		     day_index_start = date.indexOf("(", day_index_start + 1);
+		     day_index_end = date.indexOf(")", day_index_end + 1);
+		
+		     if (day_index_start != -1) {
+			     String day = date.substring(day_index_start + 1, day_index_end);
+				    
+					     cost_Lotus += Lotus.getRegularWeekDay();
+					     cost_RadhaKirshna += RadhaKirshna.getRegularWeekDay();
+					     cost_Ganesh += Ganesh.getRegularWeekDay();
+				     
+			     }
+		     }  
+	    
+	     System.out.println("Lotus : "+cost_Lotus+"$");
+	     System.out.println("RadhaKirshna : "+cost_RadhaKirshna+"$");
+	     System.out.println("Ganesh: "+cost_Ganesh+"$");
+	
+	     String result = min(cost_Lotus, cost_RadhaKirshna, cost_Ganesh);
+	     System.out.println("Cheapest price is of "+result);
+    	}
+    	
+  //implementing the min method
+    private static String min(int x, int y, int z) {
+	     if (x < y && x < z) {
+	    	 return Lotus.getHotelName();
+	     } else if (y < x && y < z) {
+	    	 return RadhaKirshna.getHotelName();
+	     } else if (z < x && z < y) {
+	    	 return Ganesh.getHotelName();
+	     } else if (x == y || y == z) {
+	    	 return Ganesh.getHotelName();
+	     } else if (z == x) {
+	    	 return RadhaKirshna.getHotelName();
+	     } else {
+	    	 return Lotus.getHotelName();
+	     } 
+	}
 }
+
 
